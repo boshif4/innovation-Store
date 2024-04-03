b6c9db3c35a9b95ce9799e0ed5a15f6861644169
boshif4 <boshif4@gmail.com>
2024-04-02 19:21:15 -0700

    first commit

---
+++ Shopping-App/src/components/About.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function About() {
+  return (
+    <div>
+      <div className="container py-5 my-5">
+        <div className="row">
+          <div className="col-md-6">
+            <h1 className="fw-bold mb-4 dark-text">About Us</h1>
+            <p className="lead mb-4">
+            "World of Innovation Shopping" is a specialized store offering a diverse and distinctive range of innovative products. 
+            We strive to provide a unique shopping platform that combines attractive designs, modern technology, 
+            and creative solutions to meet the needs and interests of our customers. 
+            Our store features a wide selection of products including smart devices, unique gifts, modern home accessories, and tools for innovative hobbies and activities. 
+            We are committed to providing excellent customer service and a comfortable and enjoyable shopping experience for every visitor to our store.
+            </p>
+           
+          </div>
+        
+         
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
